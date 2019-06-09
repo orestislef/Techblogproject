56657b4dbf41d98d200ee71df25fc39bf84d2d91
orestislef <orestis_94@hotmail.com>
2019-06-09 18:26:16 +0300

    *Navigation Swipe from left

--- app/src/main/java/com/orestislef/techblogproject/MainActivity.java
+++ app/src/main/java/com/orestislef/techblogproject/MainActivity.java
@@ -1,17 +1,28 @@
 package com.orestislef.techblogproject;
 
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
+import java.lang.ref.Reference;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -21,7 +32,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     RecyclerView NewsRecyclerview;
     RecyclerViewPostAdapter recyclerViewPostAdapter;
@@ -32,8 +43,13 @@ public class MainActivity extends AppCompatActivity {
     CharSequence search = "";
 
     public int postsPerPage = 30;
+    public int category = 0;
     private static final String TAG = "MainActivity";
     public String imageUrl;
+
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
+    private ActionBarDrawerToggle toggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,26 +65,106 @@ public class MainActivity extends AppCompatActivity {
 
         // hide the action bar
 
-        getSupportActionBar().hide();
+//        getSupportActionBar().hide();
 
 
         // ini view
 
-        rootLayout = findViewById(R.id.root_layout);
+//        rootLayout = findViewById(R.id.root_layout);
+        drawer = findViewById(R.id.drawer_layout);
         searchInput = findViewById(R.id.search_input);
         NewsRecyclerview = findViewById(R.id.news_rv);
         mData = new ArrayList<>();
 
         // load theme state
 
+        toolbar = findViewById(R.id.toolbar);
+        toolbar.setVisibility(View.GONE);
+        setSupportActionBar(toolbar);
+        drawer.findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+
+        getData();
+
+
+        // adapter ini and setup
+
+        recyclerViewPostAdapter = new RecyclerViewPostAdapter(this, mData, isDark);
+        NewsRecyclerview.setAdapter(recyclerViewPostAdapter);
+        NewsRecyclerview.setLayoutManager(new LinearLayoutManager(this));
+
+        searchInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                recyclerViewPostAdapter.getFilter().filter(s);
+                search = s;
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Log.d(TAG, "onNavigationItemSelected: selected: " + menuItem);
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                category = 0;
+                Toast.makeText(this, "home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_smartphones:
+                category = 8;
+                Toast.makeText(this, "smartphone", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        mData.clear();
+        getData();
+
+        drawer.closeDrawer(GravityCompat.START);
+        setSupportActionBar(toolbar);
+
+        toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else
+            super.onBackPressed();
+    }
+
+    public void getData() {
         Retrofit retrofit = new Retrofit.Builder().baseUrl(getResources()
                 .getString(R.string.base_url))
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
         RetrofitArrayApi service = retrofit.create(RetrofitArrayApi.class);
-        Call<List<WPPost>> call = service.getPostPerPage(postsPerPage);
+        Call<List<WPPost>> call;
+        if (category == 0) {
+            call = service.getPostPerPage(postsPerPage);
+        } else {
+            call = service.getPostByCategory(category);
+        }
 
         call.enqueue(new Callback<List<WPPost>>() {
             @Override
@@ -122,7 +218,8 @@ public class MainActivity extends AppCompatActivity {
                                               imageUrl = "";
                                               Log.d(TAG, "onResponseImage: " + "\n******************************************************************************************" + "\n\t null media\n" + imageUrl + "\n******************************************************************************************");
                                           }
-                                          mData.add(new PostItem(mTitle, finalMSubtitle,finalMContent, mDate, imageUrl));
+                                          mData.add(new PostItem(mTitle, finalMSubtitle, finalMContent, mDate, imageUrl));
+                                          recyclerViewPostAdapter.notifyDataSetChanged();
                                       }
 
                                       @Override
@@ -131,32 +228,11 @@ public class MainActivity extends AppCompatActivity {
                                   }
                     );
                 }
+
             }
 
             @Override
             public void onFailure(Call<List<WPPost>> call, Throwable t) {
-            }
-        });
-
-        // adapter ini and setup
-
-        recyclerViewPostAdapter = new RecyclerViewPostAdapter(this, mData, isDark);
-        NewsRecyclerview.setAdapter(recyclerViewPostAdapter);
-        NewsRecyclerview.setLayoutManager(new LinearLayoutManager(this));
-
-        searchInput.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                recyclerViewPostAdapter.getFilter().filter(s);
-                search = s;
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
             }
         });
     }
